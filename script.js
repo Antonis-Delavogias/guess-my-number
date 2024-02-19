b8273cd3bb036ed0c0125afcbaf0dd8ca0584f33
AntonisDela <antonisdelavogias@icloud.com>
2024-02-19 16:43:11 +0200

    now score is dynamically loses a value each time of user's wrong output

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const buttonCheck = document.querySelector(".check");
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 document.querySelector(".number").textContent = secretNumber;
 
+let score = 20; //this is the initial score
+
 buttonCheck.addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   console.log(guess, typeof guess);
@@ -14,7 +16,11 @@ buttonCheck.addEventListener("click", function () {
   } else if (guess > secretNumber) {
     document.querySelector(".message").textContent =
       "Too high! (Never give up)";
+    score--;
+    document.querySelector(".score").textContent = score;
   } else if (guess < secretNumber) {
     document.querySelector(".message").textContent = "Too low.. (sad face)";
+    score--;
+    document.querySelector(".score").textContent = score;
   }
 });
